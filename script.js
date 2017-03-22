565e9e5240774f09749862cd4ba0eca067f4cd6f
Naanou <nael.ramanich@etu.univ-lyon1.fr>
2017-03-22 15:42:52 +0100

    Podcast adding 1st try

--- script.js
+++ script.js
@@ -15,7 +15,20 @@ function add(){
 
 		req.onload = function(){
 			if(req.status===200){
-				var data = 
+				var xmldoc = req.responseXML;
+				var titre = xmldoc.getElementById("title");
+				var descr = xmldoc.getElementById("description");
+				var lien = xmldoc.getElementById("url");
+
+				var row = document.createElement('tr');
+				row.appendChild(document.createElement('td').innerHTML = titre);
+				row.appendChild(document.createElement('td').innerHTML = descr);
+				row.appendChild(document.createElement('td').innerHTML = lien);
+
+				document.getElementById("content").appendChild(row);
+			}
+			else{
+				console.log("Error"+req.status);
 			}
 		}
 	}
