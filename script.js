5dcb3c3c21bc59c6c3c458e2664da49e0a1cdf7f
Johann Pistorius <johann.pistorius0@gmail.com>
2017-03-22 22:41:30 +0100

    ajout de style.css

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 window.addEventListener("load",function(){
 
-	document.getElementById("submit").addEventListener("click",add);
+    document.getElementById("submit").addEventListener("click",add);
 });
 
 function add(){
-	var url = document.getElementById("url_input").value;
-	if(url!==""){
-		var req = new XMLHttpRequest();
-		req.open(url);
+    var url = document.getElementById("url_input").value;
+    if(url!==""){
+        var req = new XMLHttpRequest();
+        req.open(url);
 
-		req.onerror = function(){
-			console.log("Loading failure");
-		}
+        req.onerror = function(){
+            console.log("Loading failure");
+        }
 
-		req.onload = function(){
-			if(req.status===200){
-				var xmldoc = req.responseXML;
-				var titre = xmldoc.getElementById("title");
-				var descr = xmldoc.getElementById("description");
-				var lien = xmldoc.getElementById("url");
+        req.onload = function(){
+            if(req.status===200){
+                var xmldoc = req.responseXML;
+                var titre = xmldoc.getElementById("title");
+                var descr = xmldoc.getElementById("description");
+                var lien = xmldoc.getElementById("url");
 
-				var row = document.createElement('tr');
-				row.appendChild(document.createElement('td').innerHTML = titre);
-				row.appendChild(document.createElement('td').innerHTML = descr);
-				row.appendChild(document.createElement('td').innerHTML = lien);
+                var row = document.createElement('tr');
+                row.appendChild(document.createElement('td').innerHTML = titre);
+                row.appendChild(document.createElement('td').innerHTML = descr);
+                row.appendChild(document.createElement('td').innerHTML = lien);
 
-				document.getElementById("content").appendChild(row);
-			}
-			else{
-				console.log("Error"+req.status);
-			}
-		}
-	}
+                document.getElementById("content").appendChild(row);
+            }
+            else{
+                console.log("Error"+req.status);
+            }
+        }
+    }
 }
