43a21c992a56b2e5ec46cc04d47c0c689fab731c
Naanou <nael.ramanich@etu.univ-lyon1.fr>
2017-03-29 15:09:15 +0200

    Documentation toggle added

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
 window.addEventListener("load",function(){
 
-	document.getElementById("submit").addEventListener("click",add);
+    document.getElementById("submit").addEventListener("click",add);
+    document.getElementById("doc").addEventListener("click",doc);
 });
 
 function add(){
-	var url = document.getElementById("url_input").value;
-	if(url!==""){
-		var req = new XMLHttpRequest();
-		req.open(url);
+    var url = document.getElementById("url_input").value;
+    if(url!==""){
+        var req = new XMLHttpRequest();
+        req.open("GET",""+url);
 
-		req.onerror = function(){
-			console.log("Loading failure");
-		}
+        req.onerror = function(){
+            console.log("Loading failure");
+        }
 
-		req.onload = function(){
-			if(req.status===200){
-				var xmldoc = req.responseXML;
-				var titre = xmldoc.getElementById("title");
-				var descr = xmldoc.getElementById("description");
-				var lien = xmldoc.getElementById("url");
+        req.onload = function(){
+            if(req.status===200){
+                console.log("ddddd");
+                var xmldoc = req.responseXML;
+                var titre = xmldoc.getElementById("title");
+                var descr = xmldoc.getElementById("description");
+                var lien = xmldoc.getElementById("url");
 
-				var row = document.createElement('tr');
-				row.appendChild(document.createElement('td').innerHTML = titre);
-				row.appendChild(document.createElement('td').innerHTML = descr);
-				row.appendChild(document.createElement('td').innerHTML = lien);
+                var row = document.createElement('tr');
+                row.appendChild(document.createElement('td').innerHTML = titre);
+                row.appendChild(document.createElement('td').innerHTML = descr);
+                row.appendChild(document.createElement('td').innerHTML = lien);
 
-				document.getElementById("content").appendChild(row);
-			}
-			else{
-				console.log("Error"+req.status);
-			}
-		}
-	}
+                document.getElementById("content").appendChild(row);
+            }
+            else{
+                console.log("Error"+req.status);
+            }
+        }
+    }
+}
+function doc(){
+    if(document.getElementById("doc").checked){
+        document.getElementById("doctext").innerHTML="<pre>Documentation:\nhuhhhh</pre>";
+    }
+    else{
+        document.getElementById("doctext").innerHTML="";
+    }
 }
